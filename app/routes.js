ff719c338a47dab75b136cb5e44ad31997015760
Sarah Binney <sarah.binney@softwire.com>
2020-06-01 12:29:14 +0100

    Add skeleton "Choose working group" page

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const { frameworks } = require('./test-data/frameworks.js');
 
 router.get('/dashboard', (req, res) => {
-    res.render("dashboard", {frameworks: frameworks})
+    res.render('dashboard', {frameworks: frameworks})
 });
+
+router.post('/frameworks/new', (req, res) => {
+    console.log(req.body);
+    res.redirect('working-group');
+})
 
 module.exports = router;
