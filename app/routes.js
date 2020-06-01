1dc5290138590874d1c31dadd1bb9224136e9cfc
Sarah Binney <sarah.binney@softwire.com>
2020-06-01 13:05:28 +0100

    Add skeleton Details page

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,21 @@ router.get('/dashboard', (req, res) => {
 router.post('/frameworks/new', (req, res) => {
     console.log(req.body);
     res.redirect('working-group');
+});
+
+router.get('/frameworks/new', (req, res) => {
+    res.render('frameworks/new');
+}).get('/frameworks/working-group', (req, res) => {
+    res.render('frameworks/working-group');
 })
+
+router.get('/frameworks/:slug', (req, res) => {
+    const framework = frameworks.filter(f => f.slug === req.params.slug)[0];
+    if (!framework) {
+        res.render('404');
+    } else {
+        res.render('frameworks/details', {framework: framework});
+    }
+});
 
 module.exports = router;
--- app/test-data/frameworks.js
+++ app/test-data/frameworks.js
@@ -1,9 +1,19 @@
 const frameworks = [
     {
         "title": "My Test Framework",
+        "slug": "my-test-framework",
         "description": "Teaching learners how to do the thing, this framework provides a recognised industry accreditation in the form of the coveted Shiny Gold Start received by students at the end of the course.",
         "status": "Active",
-        "owners": ["Jake Turner", "Michael Trueman-Hall"]
+        "workingGroup": ["Jake Turner", "Michael Trueman-Hall"],
+        "owner": "Michael Trueman-Hall"
+    },
+    {
+        "title": "Learning Disabilities Transition Pathway Competency Framework",
+        "slug": "learning-disabilities-transition-p",
+        "description": "A test framework created for the prototype.",
+        "status": "Active",
+        "workingGroup": ["Jake Turner", "Michael Trueman-Hall", "David Levison", "Sarah Binney"],
+        "owner": "Jake Turner"
     }
 ];
 
